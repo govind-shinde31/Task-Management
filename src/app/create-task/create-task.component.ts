af0d033c02c6325111d5b2d7de623ff931d52223
Shyam <shyams@alohatechnology.com>
2023-08-27 18:31:15 +0530

    more changes

--- src/app/create-task/create-task.component.ts
+++ src/app/create-task/create-task.component.ts
@@ -2,8 +2,9 @@ import { Component, Input } from '@angular/core';
 import { TaskService } from '../service/task.service';
 import { Router } from '@angular/router';
 import { Task } from '../shared/task.model';
-import {faCalendar} from '@fortawesome/free-solid-svg-icons'
+import { faCalendar } from '@fortawesome/free-solid-svg-icons'
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -13,14 +14,14 @@ import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 })
 export class CreateTaskComponent {
   faCalender = faCalendar;
-  dueDate?: undefined;
-  newTask: Task = {
-    id: 0,
-    title: '',
-    description: '',
-    dueDate: undefined,
-    completed: false,
-  };
+
+
+  taskForm = new FormGroup({
+    id: new FormControl(Date.now()),
+    title: new FormControl('', Validators.required),
+    description: new FormControl(''),
+    dueDate: new FormControl('', Validators.required),
+  });
 
 
 
@@ -33,17 +34,16 @@ export class CreateTaskComponent {
 
   }
 
+  onSubmit(): void {
 
-  submitForm() {
-
-    this.newTask.id = Date.now(); // Generate a unique id (timestamp-based)
-    this.taskService.addTask(this.newTask);
+    this.taskService.addTask(this.taskForm.value as Task);
 
     this.taskService.showSuccess("Task Created", "Task Management")
     setTimeout(() => {
       this.router.navigate(['list']);
-      
+
     }, 1000);
+
   }
 
 
